5f3065cdae9fac03df12013018094c7699006e24
eday <cmi>
2018-12-13 11:11:02 +0800

    交换式排序： 快速排序与冒泡排序

--- Node-1/src/Main.java
+++ Node-1/src/Main.java
@@ -4,16 +4,21 @@ public class Main {
     //  数据结构 链表
     public static void main(String[] args) {
 
+        // 创建一个链表实例
         MyNode myNode = new MyNode();
-
         Node n1 = new Node("董事长");
         Node n2 = new Node("经理");
+
+        //添加节点
         myNode.add(n1);
         myNode.add(n2);
 
-
         // 打印所有节点
         myNode.display();
+
+
+        // 定位
+
 
 
     }
